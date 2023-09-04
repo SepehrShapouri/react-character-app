6ec2c0b54382b0f51f087aa7825a353937e6ae59
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2023-09-04 12:31:03 +0330

    project development resumed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 import Navbar from "./components/Navbar";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { character, episodes, allCharacters } from "./data/characters";
 import CharacterDetail from "./components/CharacterDetail";
 import CharacterList from "./components/CharacterList";
 const App = () => {
-  const [characters,setCharacters] = useState(allCharacters)
+  const [characters, setCharacters] = useState(allCharacters);
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://rickandmortyapi.com/api/character");
+      const data = await res.json();
+      setCharacters(data.results.slice(0, 5));
+    }
+    fetchData();
+  }, []);
   return (
     <div className="wrapper">
-      <Navbar numOfResult={characters.length}/>
-        <Main>
+      <Navbar numOfResult={characters.length} />
+      <Main>
         <CharacterList characters={characters} />
-        <CharacterDetail/>
-        </Main>
+        <CharacterDetail />
+      </Main>
     </div>
   );
 };
 
 export default App;
 
-function Main({children }) {
+function Main({ children }) {
   return <div className="main">{children}</div>;
 }
